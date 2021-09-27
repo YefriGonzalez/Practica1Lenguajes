8064705d7211f519f20bbdc2f126140656679a4b
Yefri González <yefrig00@gmail.com>
2021-09-27 01:26:59 -0600

    Carga de archivos realizada

---
+++ src/main/java/com/mycompany/practica_1/Main.java
@@ -0,0 +1,21 @@
+
+import com.mycompany.frames.VentanaPrincipal;
+
+/*
+
+package com.mycompany.practica_1;
+
+/**
+ *
+ * @author yefri
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // vamos a crear un nuevo objeto de 
+        //nuestra ventana y le damos
+        // un set visible
+        VentanaPrincipal ventana=new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
